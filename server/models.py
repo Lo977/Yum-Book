92b85ca45acaba803fa0a52c9bb703d35bca5427
Buddha Lo <lo_977@yahoo.com>
2025-04-04 12:58:16 -0700

    crate favorite model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,24 @@
+
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import bcrypt
+# from config import db
 from config import db
 
+db = SQLAlchemy()
+
 # Models go here!
+
+class Favorite(db.Model, SerializerMixin):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    note = db.Column(db.String) 
+
+    user = db.relationship("User", back_populates="favorites")
+    recipe = db.relationship("Recipe", back_populates="favorites")
