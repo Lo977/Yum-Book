37dc3c9049eb4722681c7edda787882e00728bc3
Buddha Lo <lo_977@yahoo.com>
2025-04-04 16:12:38 -0700

    created user and recipe model

--- server/models.py
+++ server/models.py
@@ -22,3 +22,24 @@ class Favorite(db.Model, SerializerMixin):
 
     user = db.relationship("User", back_populates="favorites")
     recipe = db.relationship("Recipe", back_populates="favorites")
+
+class User(db.Model, SerializerMixin):
+    __tablenane__ = 'users' 
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)   
+
+    recipes = db.relationship("Recipe", back_populates="user", cascade="all, delete")
+    favorites = db.relationship("Favorite", back_populates="usesr")
+
+class Recipe(db.Model,SerializerMixin):
+    __tablename__ = 'recipes'   
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.Foreign_key('users.id'))
+
+    user = db.relationship("User", back_populates="recipes")
+    favorites = db.relationship("Favorite", back_populates="recipe")
